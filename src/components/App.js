4cf30bf10216fe26ed51fad4e1ea9d53f827facd
子墨 <wx-niuqc@ibuscloud.com>
2018-11-20 16:02:26 +0800

    Add flow

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
+// @flow
 import React, {Component} from 'react';
 
 class App extends Component {
   render() {
+    const name = <div>nixing</div>
     return (
       <div>
         <h1>My React App</h1>
+        {name}
       </div>
     )
   }
